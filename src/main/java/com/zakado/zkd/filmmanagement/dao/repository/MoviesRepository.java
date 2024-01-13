edf54038355d7dbaf94a096e1e341a1e6374cac9
micha <micha2mc@gmail.com>
2024-01-13 10:09:44 +0100

    refacto y config para discovery-server

--- src/main/java/com/zakado/zkd/filmmanagement/dao/repository/MoviesRepository.java
+++ src/main/java/com/zakado/zkd/filmmanagement/dao/repository/MoviesRepository.java
@@ -1,5 +1,6 @@
 package com.zakado.zkd.filmmanagement.dao.repository;
 
+import com.zakado.zkd.filmmanagement.model.entity.Genre;
 import com.zakado.zkd.filmmanagement.model.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,6 +11,4 @@ public interface MoviesRepository extends JpaRepository<Movie, Integer> {
     List<Movie> findByTitleContainingIgnoreCase(String title);
 
     List<Movie> findByYear(Integer year);
-
-    List<Movie> findByGenresContainingIgnoreCase(String genre);
 }
